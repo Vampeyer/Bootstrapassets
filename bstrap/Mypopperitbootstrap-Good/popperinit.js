135665f3220c9afa3b1430c518b461607813b4e1
KernelOS <jthompsonprofessional0376@gmail.com>
2024-04-08 10:34:16 -0400

    bootstrap popper n jquery

---
+++ bstrap/Mypopperitbootstrap-Good/popperinit.js
@@ -0,0 +1,3 @@
+const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
+
